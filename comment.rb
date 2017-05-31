102f1874fec162c14d64f73f97be77694877ee2e
nramachandra30 <nramachandra@scu.edu>
2017-05-31 08:28:52 -0700

    Add files via upload

---
+++ comment.rb
@@ -0,0 +1,55 @@
+require 'data_mapper'
+
+class CommentData
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :comment,   String,  :required => true
+  property :created_at, DateTime
+end
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+DataMapper.finalize
+
+get '/comments' do
+  @comments = CommentData.all
+  erb :comments
+end
+
+
+get '/comments/new' do
+  if session[:admin]
+    @comment = CommentData.new
+    @comment.created_at = Time.now
+    erb :new_comment
+  else
+    erb :ask_login
+  end
+end
+
+get '/comments/:id' do
+  if session[:admin]
+    @comment = CommentData.get(params[:id])
+    erb :show_comment
+  else
+    erb :ask_login
+  end
+end
+
+post '/comments' do
+  comment = CommentData.create params[:comment]
+  puts params[:comment]
+  redirect to("/comments/#{comment.id}")
+end
+
+put '/comments/:id' do
+  comment = CommentData.get(params[:id])
+  comment.update(params[:comment])
+  redirect to("/comments/#{comment.id}")
+end
+
