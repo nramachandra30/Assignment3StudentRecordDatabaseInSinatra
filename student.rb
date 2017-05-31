849fefee40fc10520bfc63b9f2ad4a14cd300c4a
nramachandra30 <nramachandra@scu.edu>
2017-05-31 08:39:52 -0700

    Add files via upload

---
+++ student.rb
@@ -0,0 +1,79 @@
+require 'data_mapper'
+
+class StudentData
+  include DataMapper::Resource
+  property :id, Serial
+  property :sfname, String,:default => "FNU"
+  property :slname, String,:default => "FNU"
+  property :major, String,:required => true
+  property :gpa, Float
+  property :birthday, Date
+  property :address, String
+  property :studentid, String
+end
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+DataMapper.finalize
+
+get '/students' do
+  @students = StudentData.all
+  erb :students
+end
+
+get '/students/new' do
+  if session[:admin]
+    @student = StudentData.new
+    erb :new_student
+  else 
+    erb :ask_login
+  end
+end
+
+get '/students/:id' do 
+  if session[:admin]
+    @student = StudentData.get(params[:id])
+    erb :show_student
+  else
+    erb :ask_login
+  end
+end
+
+get '/students/:id/edit' do
+  if session[:admin]
+    @student = StudentData.get(params[:id])
+    erb :edit_student
+  else
+    erb :ask_login
+  end
+end
+
+post '/students' do  
+  student = StudentData.create params[:student]
+  puts params[:student]
+  redirect to("/students/#{student.id}")
+end
+
+post '/login' do
+  if (params[:username] == settings.username && params[:password] == settings.password)
+    session[:admin] = true
+    erb :home
+  else
+    erb :try_again
+  end
+end
+
+put '/students/:id' do
+  student = StudentData.get(params[:id])
+  student.update(params[:student])
+  redirect to("/students/#{student.id}")
+end
+
+delete '/students/:id' do
+  StudentData.get(params[:id]).destroy
+  redirect to('/students')
+end
